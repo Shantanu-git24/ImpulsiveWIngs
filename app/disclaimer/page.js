4d5861deefb7d0c0737c1c38b52c7d53afdc74c7
Shantanu Kolay <shantanu.new45@gmail.com>
2025-08-13 14:48:49 +0530

    Build Error

--- app/disclaimer/page.js
+++ app/disclaimer/page.js
@@ -30,12 +30,12 @@ export default function FlightsPage() {
         />
       </div>
       <NewsletterSection /> */}
-            <div className='container mx-auto max-w-7xl px-7 my-7'>
-                <p className='text-3xl mb-4 text-center'><strong>Disclaimer</strong></p>
-                <p className='mb-3'>The information contained in this website is for general information purposes only. The information is provided by [www.impulsivewings.in], a property of ImpulsiveWings Biz Llp. While we endeavour to keep the information up to date and correct, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.</p>
-                <p className='mb-3'>In no event will we be liable for any loss or damage including without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arising out of, or in connection with, the use of this website.</p>
-                <p className='mb-3'>Through this website you are able to link to other websites which are not under the control of ImpulsiveWings Biz Llp. We have no control over the nature, content and availability of those sites. The inclusion of any links does not necessarily imply a recommendation or endorse the views expressed within them.</p>
-                <p className='mb-3'>Every effort is made to keep the website up and running smoothly. However, ImpulsiveWings Biz Llp takes no responsibility for, and will not be liable for, the website being temporarily unavailable due to technical issues beyond our control.</p>
+            <div className="container mx-auto max-w-7xl px-7 my-7">
+                <p className="text-3xl mb-4 text-center"><strong>Disclaimer</strong></p>
+                <p className="mb-3">The information contained in this website is for general information purposes only. The information is provided by [www.impulsivewings.in], a property of ImpulsiveWings Biz Llp. While we endeavour to keep the information up to date and correct, we make no representations or warranties of any kind, express or implied, about the completeness, accuracy, reliability, suitability or availability with respect to the website or the information, products, services, or related graphics contained on the website for any purpose. Any reliance you place on such information is therefore strictly at your own risk.</p>
+                <p className="mb-3">In no event will we be liable for any loss or damage including without limitation, indirect or consequential loss or damage, or any loss or damage whatsoever arising from loss of data or profits arising out of, or in connection with, the use of this website.</p>
+                <p className="mb-3">Through this website you are able to link to other websites which are not under the control of ImpulsiveWings Biz Llp. We have no control over the nature, content and availability of those sites. The inclusion of any links does not necessarily imply a recommendation or endorse the views expressed within them.</p>
+                <p className="mb-3">Every effort is made to keep the website up and running smoothly. However, ImpulsiveWings Biz Llp takes no responsibility for, and will not be liable for, the website being temporarily unavailable due to technical issues beyond our control.</p>
             </div>
             <Footer />
         </main>
